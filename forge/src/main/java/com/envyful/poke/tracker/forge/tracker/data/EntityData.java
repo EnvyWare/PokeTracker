1a4d7e041f2fad1707d9efe263f6adb5daf3ad01
Daniel Norris <dnorris443@gmail.com>
2021-11-25 16:13:30 +0000

    feat(forge): add catcher record

--- forge/src/main/java/com/envyful/poke/tracker/forge/tracker/data/EntityData.java
+++ forge/src/main/java/com/envyful/poke/tracker/forge/tracker/data/EntityData.java
@@ -2,6 +2,7 @@ package com.envyful.poke.tracker.forge.tracker.data;
 
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import java.util.UUID;
@@ -13,24 +14,28 @@ public class EntityData {
     private final long spawnTime;
 
     private boolean caught;
+    private String catcher;
 
     public static EntityData of(EntityPixelmon pixelmon) {
-        return new EntityData(pixelmon.getPokemonName(), pixelmon.getUniqueID(), System.currentTimeMillis(), false);
+        return new EntityData(pixelmon.getPokemonName(), pixelmon.getUniqueID(), System.currentTimeMillis(), false,
+                              null
+        );
     }
 
     public static EntityData of(EntityPixelmon pixelmon, long time, boolean caught) {
-        return new EntityData(pixelmon.getPokemonName(), pixelmon.getUniqueID(), time, caught);
+        return new EntityData(pixelmon.getPokemonName(), pixelmon.getUniqueID(), time, caught, null);
     }
 
-    public static EntityData of(UUID uuid, String name, long time, boolean caught) {
-        return new EntityData(name, uuid, time, caught);
+    public static EntityData of(UUID uuid, String name, long time, boolean caught, String catcher) {
+        return new EntityData(name, uuid, time, caught, catcher);
     }
 
-    private EntityData(String pokemonName, UUID entityUUID, long spawnTime, boolean caught) {
+    private EntityData(String pokemonName, UUID entityUUID, long spawnTime, boolean caught, String catcher) {
         this.pokemonName = pokemonName;
         this.entityUUID = entityUUID;
         this.spawnTime = spawnTime;
         this.caught = caught;
+        this.catcher = catcher;
     }
 
     public String getPokemonName() {
@@ -55,5 +60,17 @@ public class EntityData {
 
     public void setCaught(boolean caught) {
         this.caught = caught;
+    }
+
+    public void setCatcher(EntityPlayerMP catcher) {
+        this.catcher = catcher.getName();
+    }
+
+    public String getCatcher() {
+        if (!this.caught || this.catcher == null) {
+            return "None";
+        }
+
+        return this.catcher;
     }
 }
--- forge/src/main/java/com/envyful/poke/tracker/forge/tracker/data/EntityDataTypeAdapter.java
+++ forge/src/main/java/com/envyful/poke/tracker/forge/tracker/data/EntityDataTypeAdapter.java
@@ -10,11 +10,18 @@ public class EntityDataTypeAdapter implements JsonSerializer<EntityData>, JsonDe
     @Override
     public EntityData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject object = json.getAsJsonObject();
+        String catcher = null;
+
+        if (object.has("catcher")) {
+            catcher = object.get("catcher").getAsString();
+        }
+
         return EntityData.of(
                 UUID.fromString(object.get("entityUUID").getAsString()),
                 object.get("pokemonName").getAsString(),
                 object.get("spawnTime").getAsLong(),
-                object.get("caught").getAsBoolean()
+                object.get("caught").getAsBoolean(),
+                catcher
         );
     }
 
@@ -26,6 +33,7 @@ public class EntityDataTypeAdapter implements JsonSerializer<EntityData>, JsonDe
         object.addProperty("pokemonName", src.getPokemonName());
         object.addProperty("spawnTime", src.getSpawnTime());
         object.addProperty("caught", src.isCaught());
+        object.addProperty("catcher", src.getCatcher());
 
         return object;
     }
