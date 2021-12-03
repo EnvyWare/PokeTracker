09bc12b4354c0808977ebe0c401bfe6106101584
Daniel Norris <dnorris443@gmail.com>
2021-12-03 20:37:59 +0000

    fix(forge): prevent weird spawns counting as tracked

--- forge/src/main/java/com/envyful/poke/tracker/forge/tracker/listener/PokemonSpawnListener.java
+++ forge/src/main/java/com/envyful/poke/tracker/forge/tracker/listener/PokemonSpawnListener.java
@@ -2,10 +2,10 @@ package com.envyful.poke.tracker.forge.tracker.listener;
 
 import com.envyful.api.concurrency.UtilConcurrency;
 import com.envyful.poke.tracker.forge.tracker.PokeTrackerFactory;
+import com.pixelmonmod.pixelmon.api.events.spawning.SpawnEvent;
 import com.pixelmonmod.pixelmon.entities.pixelmon.EntityPixelmon;
 import net.minecraft.entity.Entity;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class PokemonSpawnListener {
@@ -15,8 +15,8 @@ public class PokemonSpawnListener {
     }
 
     @SubscribeEvent
-    public void onPixelmonSpawn(EntityJoinWorldEvent event) {
-        Entity entity = event.getEntity();
+    public void onPixelmonSpawn(SpawnEvent event) {
+        Entity entity = event.action.getOrCreateEntity();
 
         if (!(entity instanceof EntityPixelmon)) {
             return;
