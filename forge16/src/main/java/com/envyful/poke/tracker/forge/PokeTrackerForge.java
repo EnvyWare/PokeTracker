f221ea962e9c2c2dcf5adaa44d5c17e41e0f3467
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2023-03-08 18:43:10 +0000

    chore(gradle): bump version to 1.0.5

--- forge16/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
+++ forge16/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 public class PokeTrackerForge {
 
     protected static final String MOD_ID = "poketracker";
-    protected static final String VERSION = "1.0.4";
+    protected static final String VERSION = "1.0.5";
 
     private static PokeTrackerForge instance;
 
