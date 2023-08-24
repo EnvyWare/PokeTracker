522ed846093ad7a03d3d92c12e4187083342cc23
Daniel Norris <dnorris443@gmail.com>
2023-08-24 13:46:03 +0100

    chore(gradle): bump version to 1.1.0

--- forge/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
+++ forge/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 public class PokeTrackerForge {
 
     protected static final String MOD_ID = "poketracker";
-    protected static final String VERSION = "1.0.5";
+    protected static final String VERSION = "1.1.0";
 
     @Mod.Instance(MOD_ID)
     private static PokeTrackerForge instance;
--- forge16/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
+++ forge16/src/main/java/com/envyful/poke/tracker/forge/PokeTrackerForge.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 public class PokeTrackerForge {
 
     protected static final String MOD_ID = "poketracker";
-    protected static final String VERSION = "1.0.5";
+    protected static final String VERSION = "1.1.0";
 
     private static PokeTrackerForge instance;
 
